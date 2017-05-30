a817e1e02691542a861c02bc0f9aa79439a7d873
Massoud Afrashteh <massoud.afrashteh@gmail.com>
2017-05-30 14:44:52 +0430

    change package and project names

---
+++ java/spring-boot-restful/restful/src/main/java/com/massoudafrashteh/code/restful/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.massoudafrashteh.code.restful.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+	@GetMapping("ping")
+	public String ping() {
+		return "pong";
+	}
+}
---
+++ java/spring-boot-restful/restful/src/main/java/starter/Starter.java
@@ -0,0 +1,11 @@
+package starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter {
+	public static void main(final String[] args) {
+		SpringApplication.run(Starter.class, args);
+	}
+}
