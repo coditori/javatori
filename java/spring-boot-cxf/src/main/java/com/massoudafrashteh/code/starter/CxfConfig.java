35e0f326b8dde1ce90d3866143fc4f38cfd0a763
Massoud <massoud.afrashteh@gmail.com>
2018-04-07 16:26:09 +0430

    update readme

--- java/spring-boot-cxf/src/main/java/com/massoudafrashteh/code/starter/CxfConfig.java
+++ java/spring-boot-cxf/src/main/java/com/massoudafrashteh/code/starter/CxfConfig.java
@@ -7,20 +7,12 @@ import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Arrays;
 
 @Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com")
-@EntityScan(basePackages = "com.massoudafrashteh.code.domain")
-@EnableJpaRepositories(basePackages = "com.massoudafrashteh.code.repository")
 public class CxfConfig {
 
     @Autowired
